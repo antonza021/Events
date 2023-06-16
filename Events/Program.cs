6063c997d3b829e146417ec94193e543391f8cd9
User <User@WIN-AVGURKPU8OU>
2023-06-17 00:15:26 +0700

    Добавьте файлы проекта.

---
+++ Events/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var robot = new Robot();
+
+            robot.Message += ReverseDirection;
+
+            for (int i = 0; i < 10; i++) robot.InitDirection();
+        }
+
+        static void ReverseDirection()
+        {
+            using StreamWriter sw = new StreamWriter(@"C:\Users\levch\OneDrive\Рабочий стол\log.txt", true,
+                System.Text.Encoding.Default);
+            sw.WriteLine("Назад");
+        }
+    }
+}
---
+++ Events/Robot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Robot
+    {
+        public delegate void Direction();
+
+        public event Direction? Message;
+        int GetDirection() => new Random().Next(5);
+
+        public void InitDirection()
+        {
+            switch (GetDirection())
+            {
+                case 0:
+                    Message?.Invoke();
+                    break;
+                case 1:
+                    Console.WriteLine("Вперёд");
+                    break;
+                case 2:
+                    Console.WriteLine("Влево");
+                    break;
+                case 3:
+                    Console.WriteLine("Вправо");
+                    break;
+            }
+        }
+    }
+}
