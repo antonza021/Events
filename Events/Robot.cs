089333c79c527c999e6e1bfe1fd2c73abe33296e
antonza021 <112742601+antonza021@users.noreply.github.com>
2023-06-17 13:38:34 +0700

    Robot.cs

--- Events/Robot.cs
+++ Events/Robot.cs
@@ -8,12 +8,12 @@ namespace Events
 {
     internal class Robot
     {
-        public delegate void Direction();
+        public delegate void Direction(); //Определение нового типа делегата для события. Делегат Direction не принимает параметров и не возвращает значения
 
-        public event Direction? Message;
-        int GetDirection() => new Random().Next(5);
+        public event Direction? Message; //Определение события Message с типом делегата Direction
+        int GetDirection() => new Random().Next(5); //Определение метода GetDirection, который возвращает случайное число от 0 до 4
 
-        public void InitDirection()
+        public void InitDirection() //Метод InitDirection генерирует случайное число от 0 до 4 и вызывает событие Message при наличии подписчиков
         {
             switch (GetDirection())
             {
@@ -28,7 +28,9 @@ namespace Events
                     break;
                 case 3:
                     Console.WriteLine("Вправо");
-                    break;
+                    break; //Оператор switch проверяет значение, возвращаемое методом GetDirection().
+                           //Если результат метода равен 0, генерируется событие Message. 
+                           //В других случаях метод выдает на экран одну из строк: Вперед, Влево или Вправо.
             }
         }
     }
