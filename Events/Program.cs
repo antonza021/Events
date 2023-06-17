a110d1100f44b714bcf4437894ff40a78e835af1
antonza021 <112742601+antonza021@users.noreply.github.com>
2023-06-17 13:33:23 +0700

    Program.cs

--- Events/Program.cs
+++ Events/Program.cs
@@ -8,14 +8,15 @@ namespace Events
         {
             var robot = new Robot();
 
-            robot.Message += ReverseDirection;
+            robot.Message += ReverseDirection; //Создание экземпляра класса Robot. Добавление обработчика события Message в виде метода ReverseDirection.
 
-            for (int i = 0; i < 10; i++) robot.InitDirection();
+            for (int i = 0; i < 10; i++) robot.InitDirection(); //Цикл десяти итераций. В каждой итерации вызывается метод InitDirection у объекта класса Robot.
+
         }
 
-        static void ReverseDirection()
+        static void ReverseDirection() //Метод ReverseDirection, который записывает строку в файл
         {
-            using StreamWriter sw = new StreamWriter(@"C:\Users\levch\OneDrive\Рабочий стол\log.txt", true,
+            using StreamWriter sw = new StreamWriter(@"C:\Users\levch\OneDrive\Рабочий стол\log.txt", true, //Запись строки в файл log.txt с помощью StreamWrite. FileType.Append задает режим дозаписи в файл. Кодировка файла указывается как System.Text.Encoding.Default.
                 System.Text.Encoding.Default);
             sw.WriteLine("Назад");
         }
